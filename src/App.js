41bf451f94446ad94a164038dc3fb9a084968200
Darc99 <lennyyhugoh@gmail.com>
2020-09-24 02:20:26 +0100

    added search params

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.css";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
-      <h1> Adopt Me! </h1> <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="cat" breed="Mix" />
+      <h1> Adopt Me! </h1>
+      <SearchParams />
     </div>
   );
 };
+
 export default App;
